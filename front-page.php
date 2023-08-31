c83e5d1f44580b50a14aabc05ea9d2bf11801de6
Michel Habib <r.michel@abl-outsourcing.com>
2023-08-31 09:43:02 +0300

    Insertion du style

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
 <?php
 
 get_header();
-get_template_part('template-parts/post/post', 'all');
+//get_template_part('template-parts/post/post', 'all');
--- template-parts/post/post-all.php
+++ template-parts/post/post-all.php
@@ -1,6 +1,18 @@
 <?php
+//Start the main loop
 if(have_posts()):
     while(have_posts()):the_post();
-        the_title('<h2>','</h2>');
+        if(in_category(3)):
+            the_title('<h1 style="color:red">','</h1>');
+        else:
+            the_title('<h2 style="color:green">','</h2>');    
+        endif;    
     endwhile;
 endif;
+
+//Use rewind_posts() to use the query a second time.
+rewind_posts();
+
+while(have_posts()):the_post();
+    the_post_thumbnail();
+endwhile;
